1edc87c423907f3ac2957f0937eea9cdfced690e
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-03-27 16:16:44 -0300

    Buttons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,14 +5,18 @@ export default function Home() {
     <div className="background">
       <div className="container">
         <h1>Your place to talk</h1>
-        <center><p>
-          Whether you’re part of a school club, gaming group, worldwide art
-          community, or just a handful of friends that want to spend time
-          together, Discord makes it easy to talk every day and hang out more
-          often.
-        </p></center>
-        <button>Download for Windows</button>
-        <button>Open Discord in your Browser</button>
+        <center>
+          <p>
+            Whether you’re part of a school club, gaming group, worldwide art
+            community, or just a handful of friends that want to spend time
+            together, Discord makes it easy to talk every day and hang out more
+            often.
+          </p>
+        </center>
+        <div className="buttons">
+          <button>Download for Windows</button>
+          <button>Open Discord in your Browser</button>
+        </div>
       </div>
     </div>
   );
