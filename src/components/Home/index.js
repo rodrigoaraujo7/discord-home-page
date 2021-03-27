299325b7cebbee10483ee710be7df0b412f968e5
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-03-27 16:07:43 -0300

    Font Family & Subtitle text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="background">
       <div className="container">
         <h1>Your place to talk</h1>
-        <p>
+        <center><p>
           Whether you’re part of a school club, gaming group, worldwide art
           community, or just a handful of friends that want to spend time
           together, Discord makes it easy to talk every day and hang out more
           often.
-        </p>
+        </p></center>
         <button>Download for Windows</button>
         <button>Open Discord in your Browser</button>
       </div>
