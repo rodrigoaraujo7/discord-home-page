3b843df85eb1d5f4a2f009d7a440f9c68f653070
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-03-27 16:00:19 -0300

    Text Container

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,19 @@
-import './style.css';
+import "./style.css";
 
 export default function Home() {
-    return (
-        <div className="background">
-        </div>
-    )
+  return (
+    <div className="background">
+      <div className="container">
+        <h1>Your place to talk</h1>
+        <p>
+          Whether you’re part of a school club, gaming group, worldwide art
+          community, or just a handful of friends that want to spend time
+          together, Discord makes it easy to talk every day and hang out more
+          often.
+        </p>
+        <button>Download for Windows</button>
+        <button>Open Discord in your Browser</button>
+      </div>
+    </div>
+  );
 }
