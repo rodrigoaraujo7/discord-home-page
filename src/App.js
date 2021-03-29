430915be9d83bb748fa2001ba2ce3ee92369bdd1
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-03-29 18:28:39 -0300

    Header container & discord logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './global.css'
 import Home from './components/Home';
+import Header from './components/Header'
 
 function App() {
   return (
-    <Home />
+    <>
+      <Header />
+      <Home />
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+export default function Header() {
+    return (
+        <header>
+            <img src="https://lh3.googleusercontent.com/proxy/kYhU302r-86aqYPU3zwQ5o8X90_HPYd4CShi8akjzhRi2UaXwqD9vkqlCsPAsOzXr88sHPOjjQOJtSsjLKMot886nw-T1f2uhkfV1Ff6P1V2_ieF4nABektYDAS1Q5w2-zC9Nhh7MXc" />
+        </header>
+    )
+}
