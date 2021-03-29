46020002ce30bd8e83e64022b28bc710ba041903
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-03-29 18:36:12 -0300

    Hyper links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,17 @@
-import './style.css';
+import "./style.css";
 
 export default function Header() {
-    return (
-        <header>
-            <img src="https://lh3.googleusercontent.com/proxy/kYhU302r-86aqYPU3zwQ5o8X90_HPYd4CShi8akjzhRi2UaXwqD9vkqlCsPAsOzXr88sHPOjjQOJtSsjLKMot886nw-T1f2uhkfV1Ff6P1V2_ieF4nABektYDAS1Q5w2-zC9Nhh7MXc" />
-        </header>
-    )
+  return (
+    <header>
+      <img src="https://lh3.googleusercontent.com/proxy/kYhU302r-86aqYPU3zwQ5o8X90_HPYd4CShi8akjzhRi2UaXwqD9vkqlCsPAsOzXr88sHPOjjQOJtSsjLKMot886nw-T1f2uhkfV1Ff6P1V2_ieF4nABektYDAS1Q5w2-zC9Nhh7MXc" />
+      <div className="hyperlinks">
+        <a href="#!">Download</a>
+        <a href="#!">Why Discord?</a>
+        <a href="#!">Nitro</a>
+        <a href="#!">Satefy</a>
+        <a href="#!">Support</a>
+      </div>
+      <button>Login</button>
+    </header>
+  );
 }
