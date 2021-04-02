9c752d67975fb4564440f2a280f3d54d99af0337
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-02 10:59:37 -0300

    More cards

--- src/components/Textarea/index.js
+++ src/components/Textarea/index.js
@@ -5,12 +5,30 @@ import image3 from './image3.svg';
 
 export default function Textarea() {
     return (
-        <div className="containerText">
-            <img src={image1} />
-            <div className="text">
-                <h1>An invite-only place with plenty of room to talk</h1>
-                <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
+        <>
+            <div className="containerText">
+                <img src={image1} />
+                <div className="text">
+                    <h1>An invite-only place with plenty of room to talk</h1>
+                    <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
+                </div>
             </div>
-        </div>
-    )
+
+            <div className="containerText">
+                <div className="text">
+                    <h1>An invite-only place with plenty of room to talk</h1>
+                    <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
+                </div>
+                <img src={image2} />
+            </div>
+
+            <div className="containerText">
+                <img src={image3} />
+                <div className="text">
+                    <h1>An invite-only place with plenty of room to talk</h1>
+                    <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</p>
+                </div>
+            </div>
+        </>
+    );
 }
