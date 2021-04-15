5caa800cf0c8999b5d9ac7062eccc0130479aee0
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-15 17:37:32 -0300

    New Icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,17 @@
 import './style.css'
+import { AiOutlineTwitter, AiOutlineInstagram, AiFillFacebook, AiFillYoutube } from 'react-icons/ai';
 
 export default function Footer() {
     return (
         <footer>
             <div className="social-medias">
                 <h1>Your place to talk</h1>
+                <ul>
+                    <li><AiOutlineTwitter /></li>
+                    <li><AiOutlineInstagram /></li>
+                    <li><AiFillFacebook /></li>
+                    <li><AiFillYoutube /></li>
+                </ul>
             </div>
             <div></div>
         </footer>
