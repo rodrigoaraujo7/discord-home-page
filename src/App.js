e87bb47b16f493a217c243388682d98a37a384fc
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-15 17:16:54 -0300

    Footer Container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './global.css'
 import Home from './components/Home';
 import Header from './components/Header';
 import Textarea from './components/Textarea'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <Textarea />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            
+        </footer>
+    )
+}
