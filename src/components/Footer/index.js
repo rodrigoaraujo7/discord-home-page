e1a9d7e1f1b85064b649c8fc5bdb734980b2b84d
rodrigoaraujo7 <rodrigoleitearaujo777@gmail.com>
2021-04-15 17:29:55 -0300

    Install Icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,10 @@ import './style.css'
 export default function Footer() {
     return (
         <footer>
-            
+            <div className="social-medias">
+                <h1>Your place to talk</h1>
+            </div>
+            <div></div>
         </footer>
     )
 }
